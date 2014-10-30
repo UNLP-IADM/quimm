e3e33a4c2687ef51a349c326b293108614840c17
federico rufrancos <fde.ruf@gmail.com>
2014-10-30 20:45:25 -0300

    snap derecho
    - corrección de la identación del archivo implementación.js
    - se agrego evento onclickMarker, el cual carga algunos datos en el snap
    derecho

--- src/implementacion.js
+++ src/implementacion.js
@@ -1,17 +1,26 @@
 var modelo = (function ($) {
+	var
+		map,
+		markers,
+		ultimoMark,
+		
+		//me guardo la latitud y longitud de cuando hago doble click para crear 
+		//el objeto Marcador despues
+		miLat,
+		miLng,
+		tempLatLng,
+		
+		//para el manejo de los marcadores
+		iconos,
+		goodIcon,
+		neutraIcon,
+		badIcon;
 
-    var map = L.map('map', {
-        doubleClickZoom: false
-    });
-
-    var markers = new Array();
-    var ultimoMark;
-	var miLat;
-	var miLng;
-	//me guardo la latitud y longitud de cuando hago doble click para crear el objeto Marcador despues
-	var tempLatLng;	
-	
-	var iconos = L.Icon.extend({
+	map = L.map('map', {
+		doubleClickZoom: false
+  });
+  markers = new Array();
+ 	iconos = L.Icon.extend({
 		options: {
 			iconSize:     [43, 50], // size of the icon
 			shadowSize:   [50, 64], // size of the shadow
@@ -20,80 +29,79 @@ var modelo = (function ($) {
 			popupAnchor:  [-2, -13] // point from which the popup should open relative to the iconAnchor
 		}
 	});
+	goodIcon = new iconos({iconUrl: 'imagenes/flag-export.png'});
+  neutralIcon = new iconos({iconUrl: 'imagenes/smiley_neutral.png'});
+  badIcon = new iconos({iconUrl: 'imagenes/caution.png'});
 		
-	var goodIcon = new iconos({iconUrl: 'imagenes/flag-export.png'}),
-    neutralIcon = new iconos({iconUrl: 'imagenes/smiley_neutral.png'}),
-    badIcon = new iconos({iconUrl: 'imagenes/caution.png'});
-		
-    cargarMapaPrivada = function () {
-        //var map = L.map('map').setView([-34.532, -58.53], 12);
+  cargarMapaPrivada = function () {
+		//var map = L.map('map').setView([-34.532, -58.53], 12);
 
-        //centra el mapa donde estas ubicado
-        map.locate({setView: true, maxZoom: 16});
-        L.tileLayer('http://{s}.tiles.mapbox.com/v3/federicoruf.jl3l85oh/{z}/{x}/{y}.png', {
-            maxZoom: 18
-        }).addTo(map);
+    //centra el mapa donde estas ubicado
+    map.locate({setView: true, maxZoom: 16});
+    L.tileLayer('http://{s}.tiles.mapbox.com/v3/federicoruf.jl3l85oh/{z}/{x}/{y}.png', {
+			maxZoom: 18
+    }).addTo(map);
 
-        var marker = L.marker([-34.9343485,-57.9402335]).on('click', onClick);
-        function onClick(e) {
-            snapper.open('right');
-        }
-        marker.addTo(map)
+    map.on('locationfound', onLocationFound);
+    map.on('dblclick', onMapDoubleClick);
+		//-----------------
+		//map.on('click',onMapClick);
+		//-----------------
+  }
 
-        map.on('locationfound', onLocationFound);
-        map.on('dblclick', onMapDoubleClick);
-				//-----------------
-				//map.on('click',onMapClick);
-				//-----------------
-    }
-
-    //función que retorna la posición actual donde se esta posicionado y creo un punto en esta
-    onLocationFound = function (e) {
-        console.log(e.latlng.lng);
-				miLat=e.latlng.lng;
-				miLng=e.latlng.lat;
-        console.log(e.latlng.lat);
-        var radius = e.accuracy / 2;
-        L.marker(e.latlng).addTo(map)
-            .bindPopup("You are within " + radius + " meters from this point").openPopup();
-        L.circle(e.latlng, radius).addTo(map);
+  //función que retorna la posición actual donde se esta posicionado y creo un punto en esta
+  onLocationFound = function (e) {
+    console.log(e.latlng.lng);
+		miLat=e.latlng.lng;
+		miLng=e.latlng.lat;
+    console.log(e.latlng.lat);
+    var radius = e.accuracy / 2;
+      L.marker(e.latlng).addTo(map).bindPopup("You are within " + radius + " meters from this point").openPopup();
+      L.circle(e.latlng, radius).addTo(map);
     }
 
     //en este método se debería cargar/abrir una pantalla donde se muestre el formulario
-    onMapDoubleClick = function (e) {
-
-        snapper.open('left');
-        console.log('Agregamos el evento');
+  onMapDoubleClick = function (e) {
+    snapper.open('left');
+    console.log('Agregamos el evento');
 		tempLatLng=e.latlng;		
-        /*document.getElementById("textoMarkerDivId").className = "";
-         document.getElementById("textoMarkerId").value = "";
-         markers[e.latlng] = (L.marker(e.latlng).addTo(map));
-         ultimoMark = markers[e.latlng];
-         markers[e.latlng].on('dblclick', function(e){
-         document.getElementById("textoMarkerDivId").className = "";
-         document.getElementById("textoMarkerId").value = "";
-         console.log(markers[e.latlng]);
-         ultimoMark = markers[e.latlng];
-         });*/
-    }
+	}
+	/*document.getElementById("textoMarkerDivId").className = "";
+	 document.getElementById("textoMarkerId").value = "";
+	 markers[e.latlng] = (L.marker(e.latlng).addTo(map));
+	 ultimoMark = markers[e.latlng];
+	 markers[e.latlng].on('dblclick', function(e){
+	 document.getElementById("textoMarkerDivId").className = "";
+	 document.getElementById("textoMarkerId").value = "";
+	 console.log(markers[e.latlng]);
+	 ultimoMark = markers[e.latlng];
+	 });*/
 
-		//-----------------PARA ELIMINAR, ES DE PRUBA PARA VER COMO SE VEN LOS ÍCONOS
-		//en este método se debería cargar/abrir una pantalla donde se muestre el formulario
-		/*onMapClick = function(e){
-			//alert("clickaste en " + e.latlng);
-			//document.getElementById("textoMarkerDivId").className = "";
-			//document.getElementById("textoMarkerId").value = "";
-			markers[e.latlng] = L.marker([e.latlng.lat, e.latlng.lng], {icon: neutralIcon}).addTo(map);
+
+	//-----------------PARA ELIMINAR, ES DE PRUBA PARA VER COMO SE VEN LOS ÍCONOS
+	//en este método se debería cargar/abrir una pantalla donde se muestre el formulario
+	/*onMapClick = function(e){
+		//alert("clickaste en " + e.latlng);
+		//document.getElementById("textoMarkerDivId").className = "";
+		//document.getElementById("textoMarkerId").value = "";
+		markers[e.latlng] = L.marker([e.latlng.lat, e.latlng.lng], {icon: neutralIcon}).addTo(map);
+		ultimoMark = markers[e.latlng];
+		markers[e.latlng].on('dblclick', function(e){
+			document.getElementById("textoMarkerDivId").className = "";
+			document.getElementById("textoMarkerId").value = "";
+			console.log(markers[e.latlng]);
 			ultimoMark = markers[e.latlng];
-			markers[e.latlng].on('dblclick', function(e){
-				document.getElementById("textoMarkerDivId").className = "";
-				document.getElementById("textoMarkerId").value = "";
-				console.log(markers[e.latlng]);
-				ultimoMark = markers[e.latlng];
-			});
-			L.marker([e.latlng.lat, e.latlng.lng], {icon: neutralIcon}).addTo(map);
-		}*/
-		//-----------------
+		});
+		L.marker([e.latlng.lat, e.latlng.lng], {icon: neutralIcon}).addTo(map);
+	}*/
+	//-----------------
+		
+	//formatea el texto a mostrar en el snapper de la derecha
+	onclickMarker = function(e){
+		snapper.open('right');
+		document.getElementById("descripcion").innerHTML = "<h3>Datos de marcador: "+e.latlng+"</h3>" 
+		//faltarían recuperar los datos de la base de datos(titulo, descripción, categoría)
+	}
 	
 	guardarMarcador = function () {		
 		var titulo = $("#titulo-marcador").val();
@@ -101,32 +109,38 @@ var modelo = (function ($) {
 		var categoria = $("#select-marcador").val();			
 		//lo hacemos así porque icon: pide el nombre de la variable
 		if(categoria == "goodIcon"){
-			markers[tempLatLng] = L.marker([tempLatLng.lat,tempLatLng.lng], {icon: goodIcon}).addTo(map);			
-		}else{
+			var marker = L.marker([tempLatLng.lat,tempLatLng.lng], {icon: goodIcon}).addTo(map);		
+			markers[tempLatLng] = marker;			
+			marker.on('click', onclickMarker);
+  	}else{
 			if(categoria == "neutralIcon"){
-				markers[tempLatLng] = L.marker([tempLatLng.lat,tempLatLng.lng], {icon: neutralIcon}).addTo(map);		
+				var marker = L.marker([tempLatLng.lat,tempLatLng.lng], {icon: neutralIcon}).addTo(map);		
+				markers[tempLatLng] = marker;			
+				marker.on('click', onclickMarker);
 			}else{
-				markers[tempLatLng] = L.marker([tempLatLng.lat,tempLatLng.lng], {icon: badIcon}).addTo(map);		
+				var marker = L.marker([tempLatLng.lat,tempLatLng.lng], {icon: badIcon}).addTo(map);		
+				markers[tempLatLng] = marker;			
+				marker.on('click', onclickMarker);
 			}
 		}		
-		markers[tempLatLng].bindPopup('<strong>'+titulo+'</strong><br/>'+descripcion);
+		//markers[tempLatLng].bindPopup('<strong>'+titulo+'</strong><br/>'+descripcion);
 		snapper.close();
 	}
 	
-    saveMarkerText = function () {
-        console.log("BOTON!");
-        document.getElementById("textoMarkerDivId").className = "hidden";
-        ultimoMark.bindPopup(document.getElementById("textoMarkerId").value);
-    }
+  saveMarkerText = function () {
+		console.log("BOTON!");
+    document.getElementById("textoMarkerDivId").className = "hidden";
+    ultimoMark.bindPopup(document.getElementById("textoMarkerId").value);
+  }
 		
-		irAMiPosicionPrivada = function(){		
-			console.log(miLat);
-			map.setView(new L.LatLng(miLng, miLat), 16);
-		}
+	irAMiPosicionPrivada = function(){		
+		console.log(miLat);
+		map.setView(new L.LatLng(miLng, miLat), 16);
+	}
 
-    return{
-        cargarMapa: cargarMapaPrivada,
-        saveText: saveMarkerText,
-				irAMiPosicion: irAMiPosicionPrivada
-    }
+  return{
+    cargarMapa: cargarMapaPrivada,
+    saveText: saveMarkerText,
+		irAMiPosicion: irAMiPosicionPrivada
+  }
 })(jQuery);
